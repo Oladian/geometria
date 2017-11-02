c8bfddc04297b582b1baf0ad9b68166a621e3d8d
Izan <ios2asir@gmail.com>
2017-11-02 13:51:20 +0100

    Commit 3º 	modified:   Geometria.java 	modified:   TestGeometria.java
    	Se han añadido dos métodos en Geometría.java, uno para calcular
    	el área del círculo y otro para el cálculo del área del
    	triángulo.

--- Geometria.java
+++ Geometria.java
@@ -1,11 +1,21 @@
 public class Geometria {
 
+	public static final double PI=3.1416;
+
 	public static double areaCuadrado(double lado) {
 		return lado*lado;
 	}
 
 	public static double areaRectangulo(double base, double altura) {
 		return base*altura;
+	}
+
+	public static double areaTriangulo(double base, double altura) {
+		return base*altura/2;
+	}
+
+	public static double areaCirculo(double radio){
+		return radio*radio*PI;
 	}
 
 }
--- TestGeometria.java
+++ TestGeometria.java
@@ -7,5 +7,12 @@ public class TestGeometria {
 		double areaRectangulo = Geometria.areaRectangulo(2.123,1.233);
 		System.out.println("Area del rectángulo 2.123, 1.233 vale "+ areaRectangulo);
 
+
+		double areaTriangulo = Geometria.areaTriangulo(2.123,1.233);
+		System.out.println("Area del triángulo 2.123,1.233 vale " + areaTriangulo);
+
+		double areaCirculo = Geometria.areaCirculo(1.233);
+		System.out.println("Area del círculo 1.233 vale " + areaCirculo);
+
 	}
 }
