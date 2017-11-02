c0910d762c170502e8ffdbf678b6c623f631c72e
Izan <ios2asir@gmail.com>
2017-11-02 13:21:21 +0100

    Commit Inicial, creando el respositorio

---
+++ Geometria.java
@@ -0,0 +1,11 @@
+public class Geometria {
+
+	public static double areaCuadrado(double lado) {
+		return lado*lado;
+	}
+
+	public static double areaRectangulo(double base, double altura) {
+		return base*altura;
+	}
+
+}
---
+++ TestGeometria.java
@@ -0,0 +1,11 @@
+public class TestGeometria {
+	public static void main(String[] args) {
+
+		double areaCuadrado = Geometria.areaCuadrado(2);
+		System.out.println("Area del cuadrado 2 vale "+ areaCuadrado);
+
+		double areaRectangulo = Geometria.areaRectangulo(2.123,1.233);
+		System.out.println("Area del rectángulo 2.123, 1.233 vale "+ areaRectangulo);
+
+	}
+}
